e8c3da97ada3c6fc66e9ecfca4ba73cdb422cfa0
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-06-29 01:04:21 +0800

    Configure file for firebase connection

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+export default firebase.initializeApp({
+  apiKey: "AIzaSyBvcoIxFF8MoBqqhORdrF6KFqqxLBvm1Z8",
+  authDomain: "outline-fb6ee.firebaseapp.com",
+  databaseURL: "https://outline-fb6ee.firebaseio.com",
+  projectId: "outline-fb6ee",
+  storageBucket: "outline-fb6ee.appspot.com",
+  messagingSenderId: "1007678420961",
+  appId: "1:1007678420961:web:8efb1c0644b893c22fc4a0",
+  measurementId: "G-VB2GTWJFGC",
+});
